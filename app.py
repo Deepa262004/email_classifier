12fd1ca66c3c90694efa8d663282b4144dab38af
DEEPA S <116889625+Deepa262004@users.noreply.github.com>
2025-04-24 13:52:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,5 +3,10 @@ from api import router as email_router
 
 app = FastAPI(title="Email Classifier")
 
+@app.get("/")
+def read_root():
+    return {"message": "Welcome to the Email Classifier API!"}
+
 app.include_router(email_router, prefix="/email")
+
 
